903e13fa0aaf12d66ad9ed6ca7c774b7f9c1ff72
MAHMUDUL HASAN <mh1661602@gmail.com>
2018-05-09 07:57:40 +0600

    Add files via upload

---
+++ _2d.java
@@ -0,0 +1,9 @@
+
+public class _2d {
+	public static void main(String[] args){
+		String s="TMSS college";  
+		   System.out.println(s.substring(0,4));  
+		   System.out.println(s.substring(5));
+	}
+
+}
---
+++ _2e.java
@@ -0,0 +1,10 @@
+
+public class _2e {
+	public static void main(String[] args){
+		String name="The name of my country is Bangladesh";  
+		System.out.println(name.contains("country"));  
+		System.out.println(name.contains("follow"));  
+		System.out.println(name.contains("Japan")); 
+	}
+
+}
